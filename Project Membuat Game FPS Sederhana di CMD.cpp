ef8054b059e64c7ef1f592f9d1804e354a59bd2a
Fitradana <92277117+FIDAP123@users.noreply.github.com>
2023-06-05 17:27:02 +0700

    first commit

---
+++ Project Membuat Game FPS Sederhana di CMD.cpp
@@ -0,0 +1,118 @@
+
+#include <iostream>
+#include <Windows.h>
+#include <chrono>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fFOV = 3.14159f / 4.0f;
+float fDepth = 16.0f;
+
+int main()
+{
+    wchar_t *screen = new wchar_t[nScreenWidth * nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    std::wstring map;
+
+    map += L"################";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"################";
+
+    auto tp1 = std::chrono::system_clock::now();
+    auto tp2 = std::chrono::system_clock::now();
+
+    while (1)
+    {
+        tp2 = std::chrono::system_clock::now();
+        std::chrono::duration<float> elapsedTime = tp2 - tp1;
+        tp1 = tp2;
+        float fElapsedTime = elapsedTime.count();
+
+        if (GetAsyncKeyState((unsigned short)'A') & 0x80000)
+            fPlayerA -= (0.1f) * fElapsedTime;
+
+        if (GetAsyncKeyState((unsigned short)'D') & 0x80000)
+            fPlayerA += (0.1f) * fElapsedTime;
+
+        for (int x = 0; x < nScreenWidth; x++)
+        {
+            float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+            float fDistanceToWall = 0.0f;
+            bool bHitWall = false;
+
+            float fEyeX = sinf(fRayAngle);//vector header
+            float fEyeY = cosf(fRayAngle);//vector header
+
+            while (!bHitWall && fDistanceToWall < fDepth)
+            {
+                fDistanceToWall += 0.1f;
+
+                int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+                int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+                if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
+                {
+                    bHitWall = true;
+                    fDistanceToWall = fDepth;
+                }
+                else
+                {
+                    if (map[nTestY * nMapWidth + nTestX] == '#')
+                    {
+                        bHitWall = true;
+                    }
+                }
+            }
+            
+            int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+            int nFloor = nScreenHeight - nCeiling;
+
+            short nShade = ' ';
+
+            if (fDistanceToWall <= fDepth / 4.0f)       nShade = 0x2588;
+            else if (fDistanceToWall < fDepth / 3.0f)   nShade = 0x2593;
+            else if (fDistanceToWall < fDepth / 2.0f)   nShade = 0x2593;
+            else if (fDistanceToWall < fDepth)          nShade = 0x2593;
+            else                                        nShade = ' ';
+
+            for (int y = 0; y < nScreenHeight; y++)
+            {
+                if (y <= nCeiling)
+                    screen[y * nScreenWidth + x] = ' ';
+                else if (y > nCeiling && y <= nFloor)
+                    screen[y * nScreenWidth + x] = nShade;
+                else
+                    screen[y * nScreenWidth + x] = ' ';
+            }
+        }
+
+        screen[nScreenWidth * nScreenHeight - 1] = '\0';
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+    }
+
+    //std::cout << "Hello World!\n";
+}
